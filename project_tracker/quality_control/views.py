5a17a5b63bb766d6c25025e72e39e909de511bd0
Юля Волкова <yuliyavolkova12@yandex.ru>
2024-04-25 20:45:43 +0300

    dz3 настроены урлы и вью

--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+
+def index(request):
+    another_page_url1 = reverse('quality_control:bug_list')
+    another_page_url2 = reverse('quality_control:feature_list')
+    html = f"<h1>Система контроля качества</h1><a href = '{another_page_url1}'>Список всех багов</a><br><a href = '{another_page_url2}'>Запросы на улучшение</a>"
+    return HttpResponse(html)
+
+
+def bug_list(request):
+    return HttpResponse("Список отчетов об ошибках")
+
+def feature_list(request):
+    return HttpResponse("Список запросов на улучшение")
+
+def bug_detail(request, b_id):
+    return HttpResponse(f"Детали бага {b_id}")
+
+def feature_id_detail(request, f_id):
+    return HttpResponse(f"Детали улучшения {f_id}")
