12f10020a90130ead0769c532ffbca327c186571
Юля Волкова <yuliyavolkova12@yandex.ru>
2024-04-25 19:29:53 +0300

    dz2 настроена панель админа

--- project_tracker/quality_control/admin.py
+++ project_tracker/quality_control/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Project, Task, BugReport, FeatureRequest
 
-# Register your models here.
+class TaskInline(admin.TabularInline):
+    model = Task
+    extra = 0
+    fields = ('name', 'description', 'assignee', 'status', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+    can_delete = True
+    show_change_link = True
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name', 'description')
+    ordering = ('created_at',)
+    date_hierarchy = 'created_at'
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project', 'assignee', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'assignee', 'project')
+    search_fields = ('name', 'description')
+    list_editable = ('status', 'assignee')
+    readonly_fields = ('created_at', 'updated_at')
+
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'task', 'priority', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'priority', 'project')
+    search_fields = ('title', 'description')
+    list_editable = ('status', 'priority')
+    readonly_fields = ('created_at', 'updated_at')
+    actions = ['change_status']
+
+    def change_status(self, request, queryset):
+        queryset.update(status='In_progress')
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'task', 'priority', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'priority', 'project')
+    search_fields = ('title', 'description')
+    list_editable = ('status', 'priority')
+    readonly_fields = ('created_at', 'updated_at')
