ad40161090b3a57c43d6883b6ef9a3252a8cf60b
Юля Волкова <yuliyavolkova12@yandex.ru>
2024-04-28 16:16:02 +0300

    dz4

--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -4,69 +4,46 @@ from django.urls import reverse
 from .models import BugReport, FeatureRequest
 
 def index(request):
-    another_page_url1 = reverse('quality_control:bugs_list')
-    another_page_url2 = reverse('quality_control:feature_list')
-    html = f"<h1>Система контроля качества</h1><a href = '{another_page_url1}'>Список всех багов</a><br><a href = '{another_page_url2}'>Запросы на улучшение</a>"
-    return HttpResponse(html)
+    return render (request, 'quality_control/index.html')
 
 def bugs_list(request):
     bugs = BugReport.objects.all()
-    bugs_html = '<h1>Список багов</h1><ul>'
-    for bug in bugs:
-        bugs_html += f'<li><a href="{bug.id}/">{bug.title}</a></li>'
-    bugs_html += "<ul>"
-    return HttpResponse(bugs_html)
+    return render(request, 'quality_control/bugs_list.html', {'bugs_list': bugs})
 
 def feature_list(request):
     features = FeatureRequest.objects.all()
-    feature_html = '<h1>Список фич</h1><ul>'
-    for feature in features:
-        feature_html += f'<li><a href="{feature.id}/">{feature.title}</a></li>'
-    feature_html += "<ul>"
-    return HttpResponse(feature_html)
+    return render(request, 'quality_control/feature_list.html', {'feature_list': features})
+
+def feature_detail(request, feature_id):
+    feature = get_object_or_404(FeatureRequest, id=feature_id)
+    return render(request, 'quality_control/feature_detail.html', {'feature': feature})
+def bug_detail(request, bug_id):
+    bug = get_object_or_404(BugReport, id=bug_id)
+    return render(request, 'quality_control/bug_detail.html', {'bug': bug})
 
 from django.views import View
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        bugs_list_url = reverse('quality_control:bugs_list')
-        feature_list_url = reverse('quality_control:feature_list')
-        html = f'<h1>Страница приложения quality_control</h1><a href="{bugs_list_url}">Список багов</a>'
-        html += '<br>'
-        html += f'<a href="{feature_list_url}">Список фич</a>'
-        return HttpResponse(html)
+        return render(request, 'quality_control/index.html')
 
 from django.views.generic import DetailView
 class BugDetailView(DetailView):
     model = BugReport
     pk_url_kwarg = 'bug_id'
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        bug = self.object
-        responce_html = f"<h1>Детали бага {bug.id}</h1>"
-        responce_html += f'<h2>Информация о баге</h2><h3>Название</h3><p>{bug.title}</p><h3>Описание</h3><p>{bug.description}</p><h3>Статус</h3><p>{bug.status}</p><h3>Приоритет</h3><p>{bug.priority}</p></br>'
-        responce_html += '<h2>Информация о связанном проекте</h2>'
-        responce_html += f'<h3>Название проекта</h3><p>{bug.project.name}</p><h3>Описание проекта</h3><p>{bug.project.description}</p></br>'
-        responce_html += '<h2>Информация о связанной задаче</h2>'
-        if bug.task is not None:
-            responce_html += f'<h3>Название задачи</h3><p>{bug.task.name}</p><h3>Описание задачи</h3><p>{bug.task.description}</p></br>'
-        else:
-            responce_html += '<p>Нет связанной задачи</p>'
-        return HttpResponse(responce_html)
+    template_name = 'quality_control/bug_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        bug = self.get_object()
+        context['bug'] = bug
+        return context
 
 class FeatureDetailView(DetailView):
     model = FeatureRequest
     pk_url_kwarg = 'feature_id'
-
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        feature = self.object
-        responce_html = f"<h1>Детали фичи {feature.id}</h1>"
-        responce_html += f'<h2>Информация о фиче</h2><h3>Название</h3><p>{feature.title}</p><h3>Описание</h3><p>{feature.description}</p><h3>Статус</h3><p>{feature.status}</p><h3>Приоритет</h3><p>{feature.priority}</p></br>'
-        responce_html += '<h2>Информация о связанном проекте</h2>'
-        responce_html += f'<h3>Название проекта</h3><p>{feature.project.name}</p><h3>Описание проекта</h3><p>{feature.project.description}</p></br>'
-        responce_html += '<h2>Информация о связанной задаче</h2>'
-        if feature.task is not None:
-            responce_html += f'<h3>Название задачи</h3><p>{feature.task.name}</p><h3>Описание задачи</h3><p>{feature.task.description}</p></br>'
-        else:
-            responce_html += '<p>Нет связанной задачи</p>'
-        return HttpResponse(responce_html)
+    template_name = 'quality_control/feature_detail.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        feature = self.get_object()
+        context['feature'] = feature
+        return context
