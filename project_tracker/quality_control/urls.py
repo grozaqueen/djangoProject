b8a3a75d215142cf1406100d99584e6e375a5e89
Юля Волкова <yuliyavolkova12@yandex.ru>
2024-04-28 14:26:35 +0300

    dz4

--- project_tracker/quality_control/urls.py
+++ project_tracker/quality_control/urls.py
@@ -4,9 +4,10 @@ from quality_control import views
 app_name = 'quality_control'
 
 urlpatterns = [
-    path('', views.index),
-    path('bug_list/', views.bug_list, name='bug_list'),
+    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('bugs_list/', views.bugs_list, name='bugs_list'),
     path('feature_list/', views.feature_list, name='feature_list'),
-    path('bug_detail/<int:b_id>', views.bug_detail, name='bug_detail'),
-    path('feature_id_detail/<int:f_id>', views.feature_id_detail, name='feature_id_detail')
+    path('bugs_list/<int:bug_id>/', views.BugDetailView.as_view(), name='bug_detail'),
+    path('feature_list/<int:feature_id>/', views.FeatureDetailView.as_view(), name='feature_detail')
 ]
